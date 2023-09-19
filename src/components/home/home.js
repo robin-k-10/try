b221f6471f1f9898931f57e7f6cecb4abb09ed19
robin-k-10 <robinxxx42@gmail.com>
2023-09-20 00:11:06 +0700

    Create React App and Add Navbar

---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="position-absolute top-50 start-50 translate-middle">
+      <h1>Welcome back</h1>
+    </div>
+  )
+}
+
+export default Home
