789f996ff73e57e275cebcd907835b504c1dede1
robin-k-10 <robinxxx42@gmail.com>
2023-09-27 01:53:26 +0700

    fix error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/navbar/navbar'
 import Home from './components/home/home'
 import Footer from './components/footer/footer'
 
- App() {
+function App() {
   return (
     <div className="App">
       <Navbar />
