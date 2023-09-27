f9d71e1d6883197bbd1c3db0c8c1902376916574
robin-k-10 <robinxxx42@gmail.com>
2023-09-27 22:16:41 +0700

    last

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
+      Learn React
       <Footer />
     </div>
   )
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,6 @@ const Home = () => {
     <div className="position-absolute top-50 start-50 translate-middle">
       {/* <div> */}
       <h1>Welcome back</h1>
-      <h2>learn react</h2>
     </div>
   )
 }
