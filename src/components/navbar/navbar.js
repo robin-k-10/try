b7f9d19a8873658cf093f4d6199b0ff2d2c8ef10
robin-k-10 <robinxxx42@gmail.com>
2023-09-27 21:59:09 +0700

    adf

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
           GOO
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavAltMarkup"
@@ -16,17 +16,17 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="/">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="/">
               Home
             </a>
-            <a class="nav-link" href="/">
+            <a className="nav-link" href="/">
               About
             </a>
-            <a class="nav-link" href="/">
+            <a className="nav-link" href="/">
               Contact
             </a>
           </div>
